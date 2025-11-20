65323a4a41e2e112df17344c1204a9b58ac8cbd3
libor-sloupensky <libor.sloupensky@seznam.cz>
2025-11-20 18:35:10 +0100

    commit

--- views/forbidden.php
+++ views/forbidden.php
@@ -1,5 +1,6 @@
 <?php
 /** @var string $message */
+// noop spacing tweak v2
 ?>
 <h1><?= htmlspecialchars((string)($title ?? 'Přístup odepřen'), ENT_QUOTES, 'UTF-8') ?></h1>
 <p><?= htmlspecialchars((string)$message, ENT_QUOTES, 'UTF-8') ?></p>
