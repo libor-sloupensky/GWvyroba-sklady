d41e8bd147abd6d79f85a7eeb60fdef996b7513a
libor-sloupensky <libor.sloupensky@seznam.cz>
2026-01-17 18:18:14 +0100

    upload verze

--- config/config.php
+++ config/config.php
@@ -9,7 +9,7 @@ return [
     'charset' => 'utf8mb4',
   ],
   'app' => [
-    'version' => '0.1.0',
+    'version' => '1.1.0',
   ],
   'google' => [
     // Vyplňte přes prostředí (nastavení hostingu) nebo lokálně mimo git.
